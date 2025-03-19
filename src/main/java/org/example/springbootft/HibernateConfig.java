116a259147dfa0449e73372e6e48d79bdba4e740
2023031301 <weiga_lin@tej.com.tw>
2025-03-19 10:02:27 +0800

    2025/03/19

--- src/main/java/org/example/springbootft/HibernateConfig.java
+++ src/main/java/org/example/springbootft/HibernateConfig.java
@@ -12,13 +12,11 @@ public class HibernateConfig {
 private Logger log = Logger.getLogger(this.getClass());
 
 
-
-
 public SessionFactory getSessionFactory() {
     org.hibernate.cfg.Configuration configuration = new org.hibernate.cfg.Configuration();
     Properties properties = new Properties();
     try {
-        properties.load(HibernateConfig.class.getResourceAsStream("/hibernate.properties"));
+        properties.load(HibernateConfig.class.getResourceAsStream(".\\hibernate.properties"));
         configuration.setProperties(properties);
     }catch (Exception e) {
         log.error(e);
